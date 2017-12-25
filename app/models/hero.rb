f9d071433590229e47d6aa30a983709c50dafd39
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-25 18:43:13 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -3,6 +3,13 @@ class Hero
   attr_accessor :name, :power, :biography
   @@HEROES = []
 
+  def initialize(hash)
+    @name = hash[:name]
+    @power = hash[:power]
+    @biography = hash[:biography]
+    
+  end
+
   def self.all
     @@HEROES << self
   end
